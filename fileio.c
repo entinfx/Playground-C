4652cd5f3df0048be8ce9593a88522c4b415cb83
entinfx <monkacry@gmail.com>
2022-08-22 22:11:12 +0400

    Initial commit.

--- fileio.c
+++ fileio.c
@@ -9,24 +9,24 @@ void fileBasics() {
     // stdout - monitor output
 
     /* Write file */
-    fPointer = fopen("/Users/konstantinshatalov/Developer/c_cpp_playgrounds/c_playground/c_playground/fileTest.txt", "w");
+    fPointer = fopen("path", "w");
     fprintf(fPointer, "Ground control to Major Tom.\nPut your helmet on!");
     fclose(fPointer);
 
     /* Edit file */
-    fPointer = fopen("/Users/konstantinshatalov/Developer/c_cpp_playgrounds/c_playground/c_playground/fileTest.txt", "r+");
+    fPointer = fopen("path", "r+");
     fseek(fPointer, 29, SEEK_SET); // move fPointer 29 spaces from beginning (SEEK_SET)
     // fprintf(fPointer, "Take your protein pills,\nand put your helmets on!");
     fputs("Take your protein pills,\nand put your helmet on!", fPointer);
     fclose(fPointer);
 
     /* Append to file */
-    fPointer = fopen("/Users/konstantinshatalov/Developer/c_cpp_playgrounds/c_playground/c_playground/fileTest.txt", "a");
+    fPointer = fopen("path", "a");
     fprintf(fPointer, "\n\t\t\t- David Bowie.");
     fclose(fPointer);
 
     /* Read file */
-    fPointer = fopen("/Users/konstantinshatalov/Developer/c_cpp_playgrounds/c_playground/c_playground/fileTest.txt", "r");
+    fPointer = fopen("path", "r");
     char *line = (char *)malloc(150 * sizeof(char)); // ???: Put it inside the loop?
     while (!feof(fPointer)) {
         fgets(line, 150, fPointer);
